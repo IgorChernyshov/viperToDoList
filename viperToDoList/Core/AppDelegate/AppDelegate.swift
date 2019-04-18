d9adc7b942ad7bde748dfd3d034b35cf550cd399
Igor Chernyshov <ichernyshov@bk.ru>
2019-04-18 15:08:31 +0300

    Organized project

---
+++ viperToDoList/Core/AppDelegate/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  viperToDoList
+//
+//  Created by Igor Chernyshov on 18/04/2019.
+//  Copyright © 2019 Igor Chernyshov. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+
+
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    
+    return true
+  }
+
+}
+
