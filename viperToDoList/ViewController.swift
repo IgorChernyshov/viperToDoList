aac2eaf4c982f218a661ed080c3e4ece07a8fb99
Igor Chernyshov <ichernyshov@bk.ru>
2019-04-18 14:37:37 +0300

    Initial Commit

---
+++ viperToDoList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  viperToDoList
+//
+//  Created by Igor Chernyshov on 18/04/2019.
+//  Copyright © 2019 Igor Chernyshov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
